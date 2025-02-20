ea4af8e1e270cc2347a3c1a9c425e3405f4b5f31
mkhathiyari <mkhathiyari007@gmail.com>
2025-02-20 10:26:25 +0530

    Add files via upload

---
+++ polymophism.java
@@ -0,0 +1,18 @@
+class user
+
+{
+	public static void Main(String[] args)
+	{
+		System.out.println(user.mohmmad(10,20));
+		System.out.println(user.mohmmad(10.5,11.5,12.8));
+	}
+		public static int mohmmad(int a,int b)
+		{
+			return a + b;
+		}
+		public static double mohmmad(double a,double b,double c)
+		{
+			return a + b;
+		}
+	
+}
